a16be2d0d920d6a6ab9a58bb4a60dcb6aee4cffd
Bahia369 <hsbcoding@gmail.com>
2021-05-24 20:43:26 +0100

    updated version
    added more code in could not find exact instructions but have more to do before finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import './App.css';
+import contacts from "./data.js";
+import Card from './Card';
 import emojis from "./data.js"
 
 
-const sampleEmoji =   {
-    "title": "Grinning",
-    "symbol": "😀",
-    "keywords": "grinning face happy smiley emotion emotion"
-};
 
 
-function App() {
-    return (
-        <div className="App">
-            <h1>Emojis</h1>
-            <div className="row">
+function createCard(contact) {
+    return ( < Card key = { contact.id }
+        name = { contact.name.first }
+        img = { contact.picture.medium }
+        tel = { contact.phone }
+        email = { contact.email }
+        location = { contact.location.country }
 
-            </div>
-        </div>
+        />
     );
 }
 
-export default App;
+// const sampleEmoji = {
+//     "title": "Grinning",
+//     "symbol": "😀",
+//     "keywords": "grinning face happy smiley emotion emotion"
+// };
+
+
+// function App() {
+//     return ( <
+//         div className = "App" >
+//         <
+//         h1 > Emojis < /h1> <
+//         div className = "row" >
+
+//         <
+//         /div> < /
+//         div >
+//     );
+// }
+
+// export default App;
