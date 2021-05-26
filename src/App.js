fc0fda75ee007f24fc14d5532da4c37ae6cca135
Bahia369 <hsbcoding@gmail.com>
2021-05-26 17:28:57 +0100

    updated
    updated version

--- src/App.js
+++ src/App.js
@@ -1,38 +1,20 @@
 import React from "react";
-import App from "./App.css";
+import "./App.css";
 import emojis from "./data.js";
-import reactDom from "react-dom";
-import contacts from "./data.js";
-import Card from "./Card";
-
-function createCard(contact) {
-  console.log(contact);
-  return (
-    <Card
-      key={contact.id}
-      name={contact.name}
-      img={contact.picture.medium}
-      tel={contact.phone}
-      email={contact.email}
-      location={contact.location.country}
-    />
-  );
-}
-
-function App() {
-  console.log(contacts);
-  return (
-    <div className="App">
-      <h1> My Address Book </h1>
-      <div className="main-container"> {contacts.map(createCard)} </div>
-    </div>
-  );
-}
 
 const sampleEmoji = {
   title: "Grinning",
   symbol: "😀",
   keywords: "grinning face happy smiley emotion emotion",
 };
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Emojis</h1>
+      <div className="row"></div>
+    </div>
+  );
+}
 
 export default App;
---
+++ src/EmojiCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Emoji = ({ face }) => {
+  <div className="Emoji"></div>;
+};
+
+export default Emoji;
